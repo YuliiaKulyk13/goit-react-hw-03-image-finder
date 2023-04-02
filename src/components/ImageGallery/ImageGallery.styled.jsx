8a0035945ba2379d264e9708da718a3dac6da637
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-04-01 23:21:43 -0400

    editing

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -21,4 +21,7 @@ export const Gallery = styled.ul`
 `;
 export const Item = styled.li`
   list-style: none;
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
 `;
